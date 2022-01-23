157c4e127137ceab74a2ee045f306c08ba609240
kindyluv <onyeukwuamara@gmail.com>
2022-01-23 14:09:47 +0100

    ..

---
+++ chapterOne/mapping/maps.go
@@ -0,0 +1,15 @@
+package mapping
+
+type Maps struct {
+	Id   int
+	Name string
+}
+
+func (mapping *Maps) Phoenix(m map[int]string, key int) (id int, details string) {
+	for id, details = range m {
+		if id == key {
+			return id, details
+		}
+	}
+	return 0, "native with this id doesnt exist"
+}
